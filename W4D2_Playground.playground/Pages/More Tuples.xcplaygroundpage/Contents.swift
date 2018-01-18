6e375f35f6c44e0a1d297dadc47f69f4751ab036
Curtis Mak <curtismak@Curtiss-MBP.hitronhub.home>
2018-01-17 16:33:20 -0800

    Updates to tuples playground with additional experiment and challenge

--- W4D2_Playground.playground/Pages/More Tuples.xcplaygroundpage/Contents.swift
+++ W4D2_Playground.playground/Pages/More Tuples.xcplaygroundpage/Contents.swift
@@ -1,12 +1,14 @@
 //: [Previous](@previous)
 
-//: ### More Tuples
-//: We already know switch statements can evaluate some value and tries to match it up with a case provided.
-//: The value that a switch statements evaluate can also be a tuple!
-//: The switch statement evaluates each value of the tuple individually and tries to find a match in the provided cases.
-//:
-//: Imagine we are rolling two six-sided dice. We can use a tuple to represent each of the dice value.
-
+/*:
+ ## More Tuples
+ We already know switch statements can evaluate some value and tries to match it up with a case provided.
+ The value that a switch statements evaluate can also be a tuple!
+ The switch statement evaluates each value of the tuple individually and tries to find a match in the provided cases.
+ */
+/*:
+ Imagine we are rolling two six-sided dice. We can use a tuple to represent each of the dice value.
+ */
 let diceRoll = (5, 6)
 
 // Our switch statement looks for the cases where the total value of the dice rolled is 11
@@ -19,14 +21,36 @@ default:
     print("This roll doesn't matter to us")
 }
 
-//: ### Challenge 5
-//: Create a switch statement that prints out which holiday it is given the month and the day to evaluate.
-//: Cover the following holidays:
-//: Christmas Day - Dec 25
-//: New Years Day - Jan 1
-//: Valentine's Day - Feb 14
-//: Halloween Day - Oct 31
-//: Otherwise, indiciate the given day is not a holiday
+/*:
+ - Experiment:
+ Try out the following cases within the switch statement. What do they do?
+ - case (_, 1)
+ - case (2, _)
+ - case (1...3, 1...3)
+ - case (let x, let y) where x == y
+ */
+
+
+/*:
+ - Callout(Challenge):
+ Create a switch statement that prints out which holiday it is given the month and the day to evaluate. Cover the following holidays:
+ - Christmas Day - Dec 25
+ - New Years Day - Jan 1
+ - Valentine's Day - Feb 14
+ - Halloween Day - Oct 31
+ - Otherwise, indiciate the given day is not a holiday
+ */
+
+
+/*:
+ - Callout(Challenge):
+ Imagine we have an X-Y Graph like this one: https://i.imgur.com/4By5cWN.png. Given an X-Y coordinate within a tuple, write a switch statement to cover the following cases and print these descriptions:
+ - point is on the x axis
+ - point is on the y axis
+ - point is inside the blue outlined box
+ - point is outside the blue outlined box
+ */
 
 
 //: [Next](@next)
+
