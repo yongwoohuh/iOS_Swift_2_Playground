e5eb85ebf1eccc997808027522c66375a9468d6b
Curtis Mak <curtismak@Curtiss-MBP.hitronhub.home>
2018-01-18 10:26:38 -0800

    Update generics playground with more experiments

--- W4D2_Playground.playground/Pages/Generics.xcplaygroundpage/Contents.swift
+++ W4D2_Playground.playground/Pages/Generics.xcplaygroundpage/Contents.swift
@@ -1,8 +1,12 @@
 //: [Previous](@previous)
 
-//: Generics
-//: Generics allow us to write flexible and reusable code that can take in any variable type.
-//: For example, if we want to write a function that prints two numbers for us, we might have to write a function for each type of number variable.
+/*:
+ ## Generics
+ Generics allow us to write flexible and reusable code that can take in any variable type. You have been using collections that use generic types. Both arrays and dictionaries use a generic type. You can define any variable type for those collections to store.
+ */
+/*:
+ If we want to write a function that prints two numbers for us, we would have to write a function for each type of number variable.
+ */
 
 func printMyTwoNumbersInt(num1: Int, num2: Int){
     
@@ -14,13 +18,9 @@ func printMyTwoNumbersDouble(num1: Double, num2: Double){
     print("My numbers are \(num1) and \(num2)")
 }
 
-func printMyTwoNumbersFloat(num1: Float, num2: Float){
-    
-    print("My numbers are \(num1) and \(num2)")
-}
-
-//: Instead, we can write a function that takes a generic type so we can print any two values
-
+/*:
+ Instead, we can write a function that takes a generic type so we can print any two values
+ */
 func printMyTwoNumbers<Element>(num1: Element, num2: Element){
     
     print("My numbers are \(num1) and \(num2)")
@@ -30,18 +30,53 @@ printMyTwoNumbers(num1: 1, num2: 1)
 printMyTwoNumbers(num1: 2.0, num2: 2.1)
 printMyTwoNumbers(num1: "three", num2: "four")
 
+/*:
+ - Experiment:
+ Now you try! Write a generic function that takes in two parameters and multiply their value together and print the result. ( Hint: You might run into an error after finishing. Continue to the next experiment to find out why!)
+ */
 
-//: You have also been using collections that use generic types. Both arrays and dictionaries use a generic type. You can define any variable type for those collections to store.
+
+/*:
+ - Experiment:
+ You might have run into error in the console regarding the multiplication operator can only be applied to numbered parameters. This makes sense as multiplying two Strings together doesn't make sense. So, we want to only take in variable types that *can* use the multiplication operator. In this case, we can limit the element type to types that conform to the `Numeric` protocol like below.
+ */
+
+func multiply<Element: Numeric>(num1: Element, num2: Element) {
+    
+}
 
 
-//: ### Challenge 6
-//: During class you saw a simple implementation of a stack where data is inserted (pushed) to the top of the stack when data is added. When data is removed (pop) from the stack, it removes the first item at the top of the stack.
-//: We will know implement a similar data structure called a "queue" as a generic.
-//: You can think of a queue like a line up at a grocery store. The first person in line finishes their transaction, and then leaves the line up. Any new customers have to start at the back of the line up.
-//: Create a "Queue" data structure with the following functions:
-//: enqueue: add an item to the queue
-//: dequeue: remove an item from the queue, and return the removed element
+/*:
+ - Experiment:
+ Update your multiplication function and test it! Try using different variable types to see what works and what doesn't.
+ */
+
+
+/*:
+ - Experiment:
+ Write a generic function that takes in two parameters. One parameter is an array of elements, and the other is one element you are trying to find in the list. Return the index where the element exists in the array. ie: Given [1,5,2,4] and '5', the returned index is 1
+ */
+/*:
+ - Note:
+ For this experiment, refrain from using the array method `indexOf`. And also the protocol `Equatable` might be useful here. Search it up to see what it's about.
+ */
+
+
+
+/*:
+ - Callout(Challenge):
+ During class you saw a simple implementation of a stack where data is inserted (pushed) to the top of the stack when data is added. When data is removed (pop) from the stack, it removes the first item at the top of the stack. We will now implement a similar data structure called a "queue" as a generic.
+ */
+/*:
+ - Note:
+ You can think of a queue like a line up at a grocery store. The first person in line finishes their transaction, and then leaves the line up. Any new customers have to start at the back of the line up.
+ 
+ Create a "Queue" data structure with the following functions:
+ - enqueue: add an item to the queue
+ - dequeue: remove an item from the queue, and return the removed element
+ */
 
 
 
 //: [Next](@next)
+
