5fdef7b7c78ea379e9bbe2c958d7a06ee449122a
Sam Meech-Ward <sam@meech-ward.com>
2018-02-04 14:04:44 -0800

    Create a welcome  page

---
+++ W4D2_Playground.playground/Pages/Welcome Back.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+/*:
+ # Lighthouse Labs
+ ### W4D2 - More Swift
+ */
+/*:
+ This is your second day learning the Swift programming language. Today we're looking at some more advanced features of the language like generics and error handling.
+ 
+ - callout(Tip):
+ Keep yesterday's playground open as a reference to the topics you learned yesterday.
+ */
+//: [Start](@next)
