eb7d7ee717050dc1e808e8cdf6c696494233df52
Sam Meech-Ward <sam@meech-ward.com>
2018-02-04 19:47:47 -0800

    Update Error Handling

--- W4D2_Playground.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
+++ W4D2_Playground.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
@@ -13,41 +13,41 @@ import Foundation
  Here we are defining our own 'Error' enum. 'Error' is a protocol part of the Apple framework. This is to indiciate a potential reason for an error to occur
  */
 enum DivideError: Error {
-    case CannotDivideByZero
+  case CannotDivideByZero
 }
 
 /*:
  This is a function that divides numbers num1 and num2. Notice the `throws` keyword in the function. It means this function is capable of 'raising' or 'throwing' an error
  */
 func divideNumbers( num1: Double, num2: Double ) throws -> Double{
+  
+  // We can to catch the error if we are dividing by zero, because this is bad!
+  if num2 == 0 {
     
-    // We can to catch the error if we are dividing by zero, because this is bad!
-    if num2 == 0 {
-        
-        // If the num2 parameter is in fact zero, let's indicate an error as occured by 'throwing' it out
-        throw DivideError.CannotDivideByZero
-    }
-    
-    // Otherwise, we can just do the basic division here
-    return num1 / num2
+    // If the num2 parameter is in fact zero, let's indicate an error as occured by 'throwing' it out
+    throw DivideError.CannotDivideByZero
+  }
+  
+  // Otherwise, we can just do the basic division here
+  return num1 / num2
 }
 
 /*:
  Now let's try our function and cause an error to be thrown. We start by wrapping the function inside a do/catch block.
  */
 do{
-    
-    // We call our 'divideNumbers' function with the keyword 'try' in front
-    let dividedAnswer = try divideNumbers(num1: 10, num2: 0)
-    print("My divided answer result: \(dividedAnswer)")
-    
-    // The 'divideNumbers' will throw an error because we are trying to divide by zero
+  
+  // We call our 'divideNumbers' function with the keyword 'try' in front
+  let dividedAnswer = try divideNumbers(num1: 10, num2: 0)
+  print("My divided answer result: \(dividedAnswer)")
+  
+  // The 'divideNumbers' will throw an error because we are trying to divide by zero
 }
-    // And here is where we 'catch' the error
+  // And here is where we 'catch' the error
 catch let error {
-    
-    // Once 'caught', we can print out the error for more information and prevents our app from crashing
-    print("An error is thrown: \(error)")
+  
+  // Once 'caught', we can print out the error for more information and prevents our app from crashing
+  print("An error is thrown: \(error)")
 }
 
 
@@ -59,7 +59,7 @@ catch let error {
 
 /*:
  - Experiment:
- Create your own errors that throw when the name provided is empty and if the age is invalid. (Hint: Even initializers can throw errors)
+ Create your own errors that throw when the name provided is empty or if the age is invalid. Go back and update the Human's initializer to throw an error when the data passed in is invalid.
  */
 
 
@@ -71,16 +71,17 @@ catch let error {
 
 /*:
  - Experiment:
- Test your Human class again but don't surround it with a do-catch block and use `try?` instead. What do you notice?
+ Test your Human class again but don't surround it with a do-catch block and use `try?` instead. What do you notice? (What is the value of the new human when an error is thrown?)
  */
 
 
 /*:
  - Experiment:
- Given the following JSON string, try to parse the string using 'JSONSerialization' - `class func jsonObject(with data: Data,
- options opt: JSONSerialization.ReadingOptions = []) throws -> Any`. Then print out each key-value.
+ Given the following JSON data, try to parse the JSON using `JSONSerialization`, then print out each key-value.
+ 
+ `class func jsonObject(with data: Data, options opt: JSONSerialization.ReadingOptions = []) throws -> Any`
  */
-let str = "{\"firstName\": \"Bob\", \"lastName\": \"Doe\", \"vehicles\": [\"car\", \"motorcycle\", \"train\"]}"
+let data = "{\"firstName\": \"Bob\", \"lastName\": \"Doe\", \"vehicles\": [\"car\", \"motorcycle\", \"train\"]}".data(using: .utf8)!
 
 
 /*:
@@ -105,16 +106,20 @@ let email: String? = "user1@lighthouselabs.ca"
 
 /*:
  - Callout(Challenge):
- Given the following HondaDealership class, finish it off by implementing a function and testing it. Write a function that sells off a chosen car for the price. Throw an error if the model doesn't exists, insufficient amount of money was given, or the car is out of stock.
+ Given the following HondaDealership class, finish it off by implementing a function and testing it. Write a function that sells off a chosen car for the price.
+ 
+ `func sellCar(model: String, offeredPrice: Int) throws`
+ 
+ Throw an error if the model doesn't exist, insufficient amount of money was given, or the car is out of stock.
  */
 class HondaDealership{
-    
-    var availableCarSupply = ["Civic" : (price: 5000, count: 5),
-                              "CRV" : (price: 7000, count: 9),
-                              "Prelude" : (price: 9000, count: 2)]
-    
-    
+  
+  var availableCarSupply = ["Civic" : (price: 5000, count: 5),
+                            "CRV" : (price: 7000, count: 9),
+                            "Prelude" : (price: 9000, count: 2)]
+  
+  
+  
 }
 
 //: [Next](@next)
-
