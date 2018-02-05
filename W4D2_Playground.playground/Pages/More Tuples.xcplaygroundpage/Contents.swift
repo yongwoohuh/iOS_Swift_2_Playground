7ab954a542e3372424d3c9fef1bfaa3181ec1ce0
Sam Meech-Ward <sam@meech-ward.com>
2018-02-04 19:39:18 -0800

    Update more tuples

--- W4D2_Playground.playground/Pages/More Tuples.xcplaygroundpage/Contents.swift
+++ W4D2_Playground.playground/Pages/More Tuples.xcplaygroundpage/Contents.swift
@@ -3,27 +3,27 @@
 /*:
  ## More Tuples
  We already know switch statements can evaluate some value and tries to match it up with a case provided.
- The value that a switch statements evaluate can also be a tuple!
+ The value that switch statements evaluate can also be a tuple!
  The switch statement evaluates each value of the tuple individually and tries to find a match in the provided cases.
  */
 /*:
- Imagine we are rolling two six-sided dice. We can use a tuple to represent each of the dice value.
+ Imagine we are rolling two six-sided dice. We can use a tuple to represent each of the dice values.
  */
 let diceRoll = (5, 6)
 
 // Our switch statement looks for the cases where the total value of the dice rolled is 11
 switch diceRoll {
 case (5, 6):
-    print("We got 11!")
+  print("We got 11!")
 case (6, 5):
-    print("We got 11!")
+  print("We got 11!")
 default:
-    print("This roll doesn't matter to us")
+  print("This roll doesn't matter to us")
 }
 
 /*:
  - Experiment:
- Try out the following cases within the switch statement. What do they do?
+ Use the below switch statement to test the following cases. What do they do? Try changing the value of `diceRoll` to see what happens!
  - case (_, 1)
  - case (2, _)
  - case (1...3, 1...3)
@@ -44,7 +44,11 @@ default:
 
 /*:
  - Callout(Challenge):
- Imagine we have an X-Y Graph like this one: https://i.imgur.com/4By5cWN.png. Given an X-Y coordinate within a tuple, write a switch statement to cover the following cases and print these descriptions:
+ Take a look at the following X-Y Graph.
+ 
+ ![x-y_graph](x-y_graph.png)
+ 
+ Given an X-Y coordinate within a tuple, write a switch statement to cover the following cases and print these descriptions:
  - point is on the x axis
  - point is on the y axis
  - point is inside the blue outlined box
@@ -53,4 +57,3 @@ default:
 
 
 //: [Next](@next)
-
