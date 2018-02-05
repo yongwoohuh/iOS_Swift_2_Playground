843938404a8fcd87f113de69ed6ce62d1598cde3
Sam Meech-Ward <sam@meech-ward.com>
2018-02-04 19:32:44 -0800

    Update more optionals

--- W4D2_Playground.playground/Pages/More Optionals.xcplaygroundpage/Contents.swift
+++ W4D2_Playground.playground/Pages/More Optionals.xcplaygroundpage/Contents.swift
@@ -1,24 +1,23 @@
 /*:
  ## More Optionals
  Let's start off with some review of optionals.
- When we have an optional variable, the variable might contain a value or it can contain nil. Optionals are represented with a '?' in front of the variable type.
-*/
-
+ When we have an optional variable, the variable might contain a value or it can contain nil. Optionals are represented with a '?' after the variable type.
+ */
 /*:
  - Experiment:
- Declare a optional Double value and set it to nil
+ Declare a optional Double value and set it to nil.
  */
 
 
 /*:
  - Experiment:
- Assign a value your optional Double
+ Assign a value your optional Double.
  */
 
 
 /*:
  - Experiment:
- Force unwrap the optional value. Why do you have to careful about force unwrapping?
+ Force unwrap the optional value. Why do you have to be careful about force unwrapping?
  */
 
 
@@ -38,9 +37,9 @@ var testData: [String?] = ["Heather", nil, "Mike", "John", nil, nil, "Bob"]
 /*:
  - Callout(Challenge):
  We want to write a function that validates form data filled in by a user. Once we encounter the first field that is blank, we want to indicate to the user that the field is blank.
- If the user has filled in everything correctly, we want to print out all information out.
+ If the user has filled in everything correctly, we want to print all information out.
  Below is some test data you can use to test your function.
-*/
+ */
 // Should pass all checks and print all information
 let username: String? = "user1"
 let password: String? = "password123"
@@ -59,25 +58,25 @@ let email: String? = "user1@lighthouselabs.ca"
 
 
 /*:
-## Guard Let
-Depending on how you finished the first challenge, the code you've written might not be as readable or optimal as it could be. So let's take a look at how we can improve this.
-A 'guard' statement is similar to an 'if', but a 'guard' statement will only run its block of code if the conditions are NOT met.
-Let's look at a simple guard example below.
-*/
+ ## Guard Let
+ Depending on how you finished the first challenge, the code you've written might not be as readable or optimal as it could be. So let's take a look at how we can improve this.
+ A 'guard' statement is similar to an 'if', but a 'guard' statement will only run its block of code if the conditions are NOT met.
+ Let's look at a simple guard example below.
+ */
 let myNumber = 10
 
 // This function checks if the number given to it is a positive or negative value
 func isMyNumberANegativeValue(myNumber: Int){
-    
-    // This guard condition checks if 'myNumber' is less than zero.
-    // Remember, if this condition IS FALSE, the block of code within the else block will run
-    // Otherwise, the function will continue
-    guard myNumber < 0 else {
-        print("I am positive")
-        return
-    }
-    
-    print("I am negative!")
+  
+  // This guard condition checks if 'myNumber' is less than zero.
+  // Remember, if this condition IS FALSE, the block of code within the else block will run
+  // Otherwise, the function will continue
+  guard myNumber < 0 else {
+    print("I am positive")
+    return
+  }
+  
+  print("I am negative!")
 }
 
 isMyNumberANegativeValue(myNumber: myNumber)
@@ -102,33 +101,33 @@ let myOptionalNumber: Int? = 10
 
 // This function just checks if the given parameter is nil or not
 func isMyNumberAnOptional(myOptionalNumber: Int?){
+  
+  // This guard checks if 'myOptionalNumber' has a value or not. Similar concept to conditional unwrapping
+  // If 'myOptionalNumber' is indeed nil, the else block will run
+  // Otherwise, our guard let statement is looking for a valid value, and the code will continue to run
+  guard let myUnwrapped = myOptionalNumber else {
     
-    // This guard checks if 'myOptionalNumber' has a value or not. Similar concept to conditional unwrapping
-    // If 'myOptionalNumber' is indeed nil, the else block will run
-    // Otherwise, our guard if let statement is looking for a valid value, and the code will continue to run
-    guard let myUnwrapped = myOptionalNumber else {
-        
-        print("This value is nil")
-        return
-    }
-    
-    // Lastly, you can see we can use the variable 'myUnwrapped' variable declared above within our entire scope of this function now
-    // Very convenient!!
-    // A regular 'if let' would not allow this
-    print("This unwrapped value is \(myUnwrapped)")
+    print("This value is nil")
+    return
+  }
+  
+  // Lastly, you can see we can use the variable 'myUnwrapped' variable declared above within our entire scope of this function now
+  // Very convenient!!
+  // A regular 'if let' would not allow this
+  print("This unwrapped value is \(myUnwrapped)")
 }
 
 isMyNumberAnOptional(myOptionalNumber: myOptionalNumber)
 
 /*:
  - Experiment:
- Create a function that takes in an array of numbers. Have the function add all the numbers together and return the result. Ensure there exists at least one value in the array using the `first` method of the array.
+ Create a function that takes in an array of numbers. Have the function add all the numbers together and return the result. Make sure to `guard` against an empty array. Use `array.first` to check if there is at least one value in the array.
  */
 
 
 /*:
  - Callout(Challenge):
-  Now that we've learnt this new guard statement, let's rewrite the form validation challenge using the guard statements. How does it improve our current implementation?
+ Now that we've learnt this new guard statement, let's rewrite the form validation challenge using the guard statements. How does it improve our current implementation?
  */
 
 
